75dcb3666f149afcd0aea2a631441651d5d203b9
rubenuijtdewilligen <ruben.uijtdewilligen07@gmail.com>
2021-12-02 17:13:35 +0100

    ♻️ Made seperate functions for moving a paddle up & down

--- src/PlayerController.java
+++ src/PlayerController.java
@@ -18,13 +18,33 @@ public class PlayerController {
      */
     public void update(double dt) {
         if (keyListener.isKeyPressed(KeyEvent.VK_DOWN)) {
-            if ((rect.y + Constants.PADDLE_SPEED * dt) + rect.height < Constants.WINDOW_HEIGHT - Constants.INSETS_BOTTOM) {
-                this.rect.y += Constants.PADDLE_SPEED * dt;
-            }
+            moveDown(dt);
         } else if (keyListener.isKeyPressed(KeyEvent.VK_UP)) {
-            if (rect.y - Constants.PADDLE_SPEED * dt > Constants.TOOLBAR_HEIGHT) {
-                this.rect.y -= Constants.PADDLE_SPEED * dt;
-            }
+            moveUp(dt);
+        }
+    }
+
+    /**
+     * Moves the paddle up
+     *
+     * @param dt Delta time
+     * @author Ruben "RenderMelon" Uijtdewilligen
+     */
+    public void moveUp(double dt) {
+        if (rect.y - Constants.PADDLE_SPEED * dt > Constants.TOOLBAR_HEIGHT) {
+            this.rect.y -= Constants.PADDLE_SPEED * dt;
+        }
+    }
+
+    /**
+     * Moves the paddle down
+     *
+     * @param dt Delta time
+     * @author Ruben "RenderMelon" Uijtdewilligen
+     */
+    public void moveDown(double dt) {
+        if ((rect.y + Constants.PADDLE_SPEED * dt) + rect.height < Constants.WINDOW_HEIGHT - Constants.INSETS_BOTTOM) {
+            this.rect.y += Constants.PADDLE_SPEED * dt;
         }
     }
 
